527773bd015a71894ee194987ee60b624cd48b65
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2023-03-28 09:01:32 +0200

    fixed some more code

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -7,42 +7,72 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+    Flask index
+    :return:
+    """
     version = flask.request.args.get("urllib_version")
     url = flask.request.args.get("url")
     return fetch_website(version, url)
 
-        
+
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
 
 
 class Person(object):
+    """
+    Describing the person
+    """
     def __init__(self, name):
         self.name = name
 
 
 def print_nametag(format_string, person):
+    """
+    Printing nametag
+    :param format_string: format string
+    :param person: person name
+    :return:
+    """
     print(format_string.format(person=person))
 
 
-def fetch_website(urllib_version_fun, url):
+def fetch_website(urllib_version_fun, url) -> None:
+    """
+    Fetching the website
+    :param urllib_version_fun: version
+    :param url: url
+    :return: None
+    """
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version_fun} as urllib", globals())
     # Fetch and print the requested URL
- 
-    try: 
-        http = urllib3.PoolManager()
-        r = http.request('GET', url)
-    except Exception:
-        print('Exception')
+
+    http = urllib3.PoolManager()
+    request_our = http.request('GET', url)
+
+    print(request_our)
 
 
-def load_yaml(filename):
-    stream = open(filename)
-    deserialized_data = yaml.load(stream, Loader=yaml.Loader)  # deserializing data
+def load_yaml(filename) -> dict:
+    """
+    Loading yaml
+    :param filename: filename
+    :return: dictionary with deserialized data
+    """
+    with open(filename, "r") as stream:
+        # deserializing data
+        deserialized_data = yaml.load(stream, Loader=yaml.Loader)
+
     return deserialized_data
 
 
-def authenticate(actual_password):
+def authenticate(actual_password) -> None:
+    """
+    Function which authenticates user
+    :param actual_password: Your password
+    :return: None
+    """
     # Assert that the password is correct
     assert actual_password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
